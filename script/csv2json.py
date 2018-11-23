f8529a26d9169d32f1d1730fd3569df4a3da4579
gspinaci <spino9330@gmail.com>
2018-11-23 12:53:18 +0100

    python script to get lat+long

--- script/csv2json.py
+++ script/csv2json.py
@@ -1,14 +1,67 @@
 import csv
+import json
+from geopy.geocoders import Nominatim
 
-output = {}
+index_list = {
+    "regione": 1,
+    'provincia': 2,
+    'codice_scuola': 5,
+    'indirizzo': 7,
+    'cap': 8,
+    'nome_scuola': 6
+}
+
+# Get
+geolocator = Nominatim(user_agent="sebucci")
+
+buildings = []
 
 # Italy
-path_percentuali_italia = '../dataset/percentuali/percentuali_italia.csv'
+path_edifici_italia = '../dataset/allnodups.csv'
+path_json_address = '../dataset/geo/italy.json'
 
-with open(path_percentuali_italia, newline='') as csvfile:
+with open(path_edifici_italia, newline='') as csvfile:
 
-    rows = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in rows:
-        print(row)
+    with open(path_json_address, 'w') as fp:
 
-    csvfile.close()
+        rows = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+        index = 1
+
+        for row in rows:
+
+            if 'SICILIA' == row[1]:
+
+                regione = row[1]
+                provincia = row[2]
+                codice_scuola = row[5]
+                nome_scuola = row[6]
+
+                indirizzo = row[7] + ' ' + row[8] + ' ' + row[2]
+
+                location = geolocator.geocode(indirizzo)
+
+                try:
+
+                    building = {
+                        "codice_scuola": codice_scuola,
+                        "nome_scuola": nome_scuola,
+                        "regione": regione,
+                        "provincia": provincia,
+                        "indirizzo": indirizzo,
+                        "posizione": {
+                            "lat": location.latitude,
+                            "lon": location.longitude
+                        }
+                    }
+                    print('done ' + index)
+                    buildings.append(building)
+                    index = index + 1
+                except:
+                    pass
+
+        csvfile.close()
+
+        json.dump(buildings, fp)
+
+        fp.close()
